f342f39107786f747058e829ec55013092b272e0
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-08-31 17:09:19 +0530

    Update f01.c

--- files/f01.c
+++ files/f01.c
@@ -1,1 +1,14 @@
+/* program to create a text file */
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+FILE *fp;
+fp=fopen("fruits.txt","w");
+fputs("Apple",fp);
+fputs("\nOrange",fp);
+fputs("\nBanana",fp);
+fclose(fp);
+printf("\nFile Created");
+}
 
