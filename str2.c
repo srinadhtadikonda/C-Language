be36e9af9b6a46a0781124d6fcc4569c4c344af4
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-09-16 19:09:56 +0530

    Update str2.c

--- str2.c
+++ str2.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
-
-int main() {
-    char s1[25] = "state bank of india";
-    int i;
-    int count = 0;  // Variable to count the number of characters
-
-    // Loop through the string and print each character, while also counting them
-    for (i = 0; s1[i] != '\0'; i++) {
-        printf("%c", s1[i]);  // Print each character
-        count++;  // Increment the counter for each character
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+main()
+{
+    char str[100];
+    int c=0,i;
+    printf("Enter A string..:");
+    scanf("%[^\n]",&str);
+    printf("\nString=%s",str);
+    for(i=0;str[i]!='\0';++i)
+    {
+    c=c+1;
     }
-
-    // After the loop, print the number of characters
-    printf("\nNumber of characters (including spaces): %d\n", count);
-
-    return 0;
+printf("\nLength of String=%d",c);
 }
